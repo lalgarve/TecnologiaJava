7fc38255121f2249ed724d4abf6e25bcd71937f8
Leila Otto Algarve <leila.algarve@al.infnet.edu.br>
2023-11-09 14:26:45 -0300

    Implementadas as operações altera, consultaPorId e getValores.

--- src/main/java/br/edu/infnet/tecnologiajava/services/bancodados/TabelaImpl.java
+++ src/main/java/br/edu/infnet/tecnologiajava/services/bancodados/TabelaImpl.java
@@ -21,6 +21,11 @@ import java.util.function.Predicate;
 public class TabelaImpl<C,V extends ValorBD<C>> implements TabelaBD<C, V> {
 
   private final Map<C,V> dadosTabela = new HashMap<>();
+  private final String nome;
+  
+  public TabelaImpl(String nome){
+    this.nome = nome;
+  }
   
   @Override
   public void adiciona(V valor) throws BancoDadosException {
@@ -40,15 +45,22 @@ public class TabelaImpl<C,V extends ValorBD<C>> implements TabelaBD<C, V> {
     if (valor==null){
       throw new BancoDadosException("A chave "+chave+" não existe na tabela.");
     }
-  }
+  } 
 
   @Override
   public void altera(V valor) throws BancoDadosException {
-    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    Objects.requireNonNull(valor, "O valor não pode ser nulo.");
+    Objects.requireNonNull(valor.getChave(), "A chave não pode ser nula.");
+    V valorOriginal = dadosTabela.get(valor.getChave());
+    if(valorOriginal==null){
+      throw new BancoDadosException("Não exite chave "+valor.getChave()+" para ser alterada.");
+    }
+    dadosTabela.put(valor.getChave(), valor);
   }
 
   @Override
   public Optional<V> consultaPorId(C chave) throws BancoDadosException {
+    Objects.requireNonNull(chave, "A chave não pode ser nula.");
     V valor = dadosTabela.get(chave);
     valor = valor==null?null:(V)valor.getClone();  
     return Optional.ofNullable(valor);
@@ -56,7 +68,9 @@ public class TabelaImpl<C,V extends ValorBD<C>> implements TabelaBD<C, V> {
 
   @Override
   public List<V> getValores() throws BancoDadosException {
-    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    return dadosTabela.values().stream()
+            .map((valor) -> (V)valor.getClone())
+            .toList();
   }
 
   @Override
--- src/test/java/br/edu/infnet/tecnologiajava/services/bancodados/TabelaImplTest.java
+++ src/test/java/br/edu/infnet/tecnologiajava/services/bancodados/TabelaImplTest.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 
 package br.edu.infnet.tecnologiajava.services.bancodados;
 
@@ -31,7 +27,7 @@ public class TabelaImplTest {
     valoresTeste = new ArrayList<>();
     valoresTesteMesmaChave = new ArrayList<>();
     
-    for(int i=0; i<7; i++){
+    for(int i=0; i<5; i++){
       ValorSemDependente valor;
       valor = new ValorSemDependente(i);
       valor.setDescricao("valor"+i);
@@ -48,7 +44,7 @@ public class TabelaImplTest {
   @Test
   public void testAdiciona() throws Exception {
     ValorSemDependente valor = valoresTeste.get(0);
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     instance.adiciona(valor);
     Optional<ValorSemDependente> resultado = instance.consultaPorId(valor.getChave());
     assertTrue(resultado.isPresent());
@@ -60,7 +56,7 @@ public class TabelaImplTest {
   public void testAdicionaMesmaChave() throws Exception {
     ValorSemDependente valor = valoresTeste.get(0);
     ValorSemDependente valorMesmaChave = valoresTesteMesmaChave.get(0);
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     instance.adiciona(valor);
     BancoDadosException excecao = assertThrows(BancoDadosException.class, ()->instance.adiciona(valorMesmaChave));
     assertEquals("A chave 0 já existe na tabela.", excecao.getMessage());
@@ -69,7 +65,7 @@ public class TabelaImplTest {
   @Test 
   public void testAdicionaChaveNula() throws Exception {
     ValorSemDependente valor = new ValorSemDependente();
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     NullPointerException excecao = assertThrows(NullPointerException.class, () -> instance.adiciona(valor));
     assertEquals("A chave não pode ser nula.", excecao.getMessage());
   }
@@ -77,7 +73,7 @@ public class TabelaImplTest {
   @Test 
   public void testAdicionaValorNulo() throws Exception {
     ValorSemDependente valor = null;
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     NullPointerException excecao = assertThrows(NullPointerException.class, () -> instance.adiciona(valor));
     assertEquals("O valor não pode ser nulo.", excecao.getMessage());
   }
@@ -85,7 +81,7 @@ public class TabelaImplTest {
   @Test
   public void testRemove() throws Exception {
     ValorSemDependente valor = valoresTeste.get(0);
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     instance.adiciona(valor);    
     instance.removePorId(valor.getChave());
     Optional<ValorSemDependente> resultado = instance.consultaPorId(valor.getChave());
@@ -94,16 +90,101 @@ public class TabelaImplTest {
 
   @Test
   public void testRemoveValorNaoExiste() throws Exception {
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     BancoDadosException excecao = assertThrows(BancoDadosException.class, () -> instance.removePorId(0));
     assertEquals("A chave 0 não existe na tabela.", excecao.getMessage());
   }
   
   @Test
   public void testRemoveChaveNula() throws Exception {
-    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
     NullPointerException excecao = assertThrows(NullPointerException.class, () -> instance.removePorId(null));
     assertEquals("A chave não pode ser nula.", excecao.getMessage());
   }  
+  
+  @Test
+  public void testAltera() throws Exception{
+    ValorSemDependente valor = valoresTeste.get(0);
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
+    instance.adiciona(valor);
+    ValorSemDependente valorAlterado = valoresTesteMesmaChave.get(0);
+    instance.altera(valorAlterado);
+    Optional<ValorSemDependente> resultado = instance.consultaPorId(valor.getChave());
+    assertTrue(resultado.isPresent());
+    assertEquals(valorAlterado, resultado.get());
+    assertFalse(valor==resultado.get(),"Um clone deveria ter sido retornado/");
+  }
+  
+  @Test
+  public void testAlteraChaveInexistente() throws Exception {
+    ValorSemDependente valor = valoresTeste.get(0);
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela"); 
+    BancoDadosException excecao = assertThrows(BancoDadosException.class, () -> instance.altera(valor));
+    assertEquals("Não exite chave 0 para ser alterada.", excecao.getMessage());
+  }
+  
+  @Test
+  public void testAlteraValorNulo() throws Exception {
+    ValorSemDependente valor = null;
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela"); 
+    NullPointerException excecao = assertThrows(NullPointerException.class, () -> instance.altera(valor));
+    assertEquals("O valor não pode ser nulo.", excecao.getMessage());
+  }  
 
+    
+  @Test
+  public void testAlteraChaveNula() throws Exception {
+    ValorSemDependente valor = new ValorSemDependente();
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela"); 
+    NullPointerException excecao = assertThrows(NullPointerException.class, () -> instance.altera(valor));
+    assertEquals("A chave não pode ser nula.", excecao.getMessage());
+  } 
+  
+  @Test
+  public void testConsultaPorIdChaveNula() throws Exception {
+    ValorSemDependente valor = valoresTeste.get(0);
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
+    instance.adiciona(valor);
+    Optional<ValorSemDependente> resultado = instance.consultaPorId(valor.getChave());
+    NullPointerException excecao = assertThrows(NullPointerException.class, () ->  instance.consultaPorId(null));
+    assertEquals("A chave não pode ser nula.", excecao.getMessage());
+  }
+  
+ 
+  @Test
+  public void testConsultaPorIdChaveNaoExiste() throws Exception {
+    ValorSemDependente valor = valoresTeste.get(0);
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
+    instance.adiciona(valor);
+    Optional<ValorSemDependente> resultado = instance.consultaPorId(valor.getChave()+1);
+    assertTrue(resultado.isEmpty());
+  } 
+  
+  @Test
+  public void testGetValores() throws Exception {
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
+    valoresTeste.forEach((valor) -> {
+      try {
+        instance.adiciona(valor);
+      } catch (BancoDadosException ex) {
+        fail(ex);
+      }
+    });
+    List<ValorSemDependente> valores = instance.getValores();
+    assertEquals(valoresTeste.size(), valores.size());
+    Optional<ValorSemDependente> naoExiste = valoresTeste.stream().filter((valorTeste) -> !valores.contains(valorTeste)).findFirst();
+    assertTrue(naoExiste.isEmpty(), "Um valor não foi encontrado: "+naoExiste.orElse(null));
+    Optional<ValorSemDependente> naoEClone = valoresTeste.stream().filter((valorTeste) -> {
+      int index = valores.indexOf(valorTeste);
+      return valores.get(index) != valorTeste;
+    }).findFirst();
+    assertTrue(naoExiste.isEmpty(), "Um valor não é clone encontrado: "+naoExiste.orElse(null));
+  }
+  
+  @Test
+  public void testGetValoresTabelaVazia() throws Exception {
+    TabelaImpl<Integer, ValorSemDependente> instance = new TabelaImpl<>("minhatabela");
+    List<ValorSemDependente> valores = instance.getValores();
+    assertEquals(0, valores.size());
+  }
 }
