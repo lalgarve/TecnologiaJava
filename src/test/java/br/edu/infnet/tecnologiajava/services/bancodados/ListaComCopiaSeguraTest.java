0aea3b2580b53dddec4a1fd1fe241554da8c632c
Leila Algarve <leila.algarve@al.infnet.edu.br>
2023-11-14 11:15:49 -0300

    Testes ListaComCopiaSegura

--- src/test/java/br/edu/infnet/tecnologiajava/services/bancodados/ListaComCopiaSeguraTest.java
+++ src/test/java/br/edu/infnet/tecnologiajava/services/bancodados/ListaComCopiaSeguraTest.java
@@ -18,7 +18,7 @@ public class ListaComCopiaSeguraTest {
     private static List<Dados> listaDados1;
     private static List<Dados> listaDados2;
 
-     private static class Dados implements Imutavel, Comparable<Dados>{
+     private static class Dados implements Imutavel{
         private final int id;
         private final  String descricao;
         Dados(int id, String descricao) {
@@ -50,14 +50,6 @@ public class ListaComCopiaSeguraTest {
             } else if (!descricao.equals(other.descricao))
                 return false;
             return true;
-        }
-        @Override
-        public int compareTo(Dados arg0) {
-            if(id!=arg0.id){
-                return id-arg0.id;
-            }else{
-                return descricao.compareTo(arg0.descricao);
-            }
         }
         
     }   
@@ -236,42 +228,50 @@ public class ListaComCopiaSeguraTest {
     }
 
     @Test
-    void testRetainAll() {
-
+    public void testRetainAll() {
+        ListaComCopiaSegura<Dados> listaOri = new ListaComCopiaSegura<>();
+        listaOri.addAll(listaDados1);
+        ListaComCopiaSegura<Dados> listaCopia = new ListaComCopiaSegura<>(listaOri); 
+        List<Dados> listaRetain = new ArrayList<>();
+        listaRetain.add(listaDados1.get(1));
+        listaRetain.add(listaDados1.get(2));
+        listaCopia.retainAll(listaRetain);
+        Object[] esperado = listaRetain.toArray();
+        Object[] resultado = listaCopia.toArray();
+        assertArrayEquals(esperado, resultado);
+        assertEquals(5, listaOri.size());
     }
 
     @Test
-    void testSet() {
-
+    public void testSet() {
+        ListaComCopiaSegura<Dados> listaOri = new ListaComCopiaSegura<>();
+        listaOri.addAll(listaDados1);
+        ListaComCopiaSegura<Dados> listaCopia = new ListaComCopiaSegura<>(listaOri); 
+        listaCopia.set(2, listaDados2.get(2));
+        assertEquals(listaDados2.get(2), listaCopia.get(2));
+        assertEquals(listaDados1.get(2), listaOri.get(2));        
     }
 
     @Test
-    void testSize() {
-
+    public void testSort() {
+        ListaComCopiaSegura<Dados> listaOri = new ListaComCopiaSegura<>();
+        for(int i=listaDados1.size()-1; i>=0; i--){
+            listaOri.add(listaDados1.get(i));
+        }
+        ListaComCopiaSegura<Dados> listaCopia = new ListaComCopiaSegura<>(listaOri);
+        listaCopia.sort((dados1, dados2) -> dados1.id - dados2.id);
+        assertArrayEquals(listaDados1.toArray(Dados[]::new), listaCopia.toArray(Dados[]::new));
+        assertEquals(listaDados1.get(4), listaOri.get(0));
     }
 
     @Test
-    void testSort() {
-
+    public void testSubList() {
+        ListaComCopiaSegura<Dados> listaOri = new ListaComCopiaSegura<>();
+        listaOri.addAll(listaDados1);
+        ListaComCopiaSegura<Dados> listaCopia = new ListaComCopiaSegura<>(listaOri); 
+        List<Dados> sublista = listaCopia.subList(1, 3);
+        sublista.set(0, listaDados2.get(0));
+        assertArrayEquals(listaDados1.toArray(Dados[]::new), listaOri.toArray(Dados[]::new));
     }
 
-    @Test
-    void testSubList() {
-
-    }
-
-    @Test
-    void testToArray() {
-
-    }
-
-    @Test
-    void testToArray2() {
-
-    }
-
-    @Test
-    void testToArray3() {
-
-    }
 }
